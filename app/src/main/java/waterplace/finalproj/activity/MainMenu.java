362c40912a0558d6292d1eeddf6c088a0d2254c8
matt <mataus94@outlook.com>
2023-06-11 19:51:45 -0300

    Pesquisa pronta

--- app/src/main/java/waterplace/finalproj/activity/MainMenu.java
+++ app/src/main/java/waterplace/finalproj/activity/MainMenu.java
@@ -6,13 +6,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Window;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.DecimalFormat;
@@ -30,7 +33,10 @@ import waterplace.finalproj.util.DistanceUtil;
 
 public class MainMenu extends AppCompatActivity {
 
+    User user = User.getInstance();
+    Address address = user.getAddresses().get(0);
     List<SupplierDistance> supplierDistances = new ArrayList<>();
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Suppliers");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +44,40 @@ public class MainMenu extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main_menu);
-        User user = User.getInstance();
-        Address address = user.getAddresses().get(0);
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Suppliers");
+        fornecedoresProximos();
 
+
+        SearchView searchView = findViewById(R.id.searchView);
+
+        // Configurar o ouvinte de eventos para a SearchView
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Executar a consulta quando o usuário pressionar o botão de pesquisa
+                performSearch(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.isEmpty()) {
+                    fornecedoresProximos();
+                } else {
+                    performSearch(newText);
+                };
+                return true;
+            }
+
+        });
+    }
+
+
+    private void fornecedoresProximos() {
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Limpar os resultados anteriores
+                supplierDistances.clear();
                 for (DataSnapshot supplierSnapshot : snapshot.getChildren()) {
                     //System.out.println("ó a id desse merda que tá bugando tudo -> " + supplierSnapshot.getKey());
                     Supplier supplier = supplierSnapshot.getValue(Supplier.class);
@@ -72,6 +105,54 @@ public class MainMenu extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(MainMenu.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void performSearch(String searchText) {
+        // Limpar a pesquisa se o texto estiver vazio
+        if (TextUtils.isEmpty(searchText)) {
+            // Limpar os resultados anteriores
+            // Atualizar a interface do usuário com os resultados da pesquisa
+            return;
+        }
+
+        // Executar a consulta no Firebase Realtime Database
+        Query query = databaseReference.orderByChild("name").equalTo(searchText);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Limpar os resultados anteriores
+                supplierDistances.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Supplier supplier = snapshot.getValue(Supplier.class);
+                    assert supplier != null;
+                    supplier.setAddress(snapshot.child("Address").getValue(Address.class));
+                    double distance;
+
+                    if (supplier.getAddress() != null) {
+                        distance = DistanceUtil.calcDistance(address.getLatitude(), address.getLongitude(), supplier.getAddress().getLatitude(), supplier.getAddress().getLongitude());
+
+                        //System.out.println(supplier.getName());
+                        //System.out.println(df.format(distance));
+                        //System.out.println("AWAWAWAAWAWAWA");
+
+                        //limite padrão para filtrar é 20km de distância
+                        if (distance <= 20.0) {
+                            String supUid = snapshot.getKey();
+                            SupplierDistance supDistance = new SupplierDistance(supplier, distance, supUid);
+                            supplierDistances.add(supDistance);
+                        }
+                    }
+
+                }
+                listSuppliers();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Lidar com erros de consulta
+                Toast.makeText(MainMenu.this, "Erro na consulta: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
